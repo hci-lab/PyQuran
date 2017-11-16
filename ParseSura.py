be32af782bd676fec0cb9d8094794100540d863d
wyousef <wyousef@fcih.net>
2017-11-16 21:41:23 +0200

    `ParseSura` to count letters in Sura

---
+++ ParseSura.py
@@ -0,0 +1,4 @@
+# Define the `ParseSura` function that takes the n^th Sura and a list of alphabets that contains m unicodes representing m characters; the default is the 29 characters (complete the initialization of the defulat value), where hamza is the 29th letter. The function parses the Sura and returns axm ndarray, A, where a is the number of verses and A[i,j] is the number of occurences of the letter alphabets[j] in verse i.
+def ParseSura(n, alphabets=['ا', 'ب']):
+    A 
+    return
