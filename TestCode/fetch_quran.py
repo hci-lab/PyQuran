8b81a0d558a9c2510a9a6612294e39f9e2139428
TahaMagdy <tahamagdy314@gmail.com>
2017-11-17 14:52:22 +0200

    update fetch_aya(): use get_sura() abstraction

--- TestCode/fetch_quran.py
+++ TestCode/fetch_quran.py
@@ -15,6 +15,7 @@ def get_sura(sura_number):
             Do not forget that the index of the reunred list
             starts at zero.
     """
+
     sura_number -= 1
     sura = []
     suras_list = quran_tree.findall('sura')
@@ -26,33 +27,33 @@ def get_sura(sura_number):
 
 
 def fetch_aya(sura_number, aya_number):
+    """
+        Input:
+            * parameter 1: the ordered number of sura in The Mushaf.
+            * parameter 2: the ordered number of aya.
+        Output:
+            * returns: aya (string)
+    """
 
-    sura_number -= 1
     aya_number -= 1
+    sura = get_sura(sura_number)
 
-    # Getting `suras` list
-    suras_list = quran_tree.findall('sura')
-
-    # Get `ayas` list
-    ayas_list = suras_list[sura_number].findall('aya')
-
-    # Elements of a tag are stored in a dictionary
-    return ayas_list[aya_number].attrib['text']
+    return sura[aya_number]
 
 
 def main():
     # testing
-#   print(fetch_aya(12, 4))
-#   print(fetch_aya(114, 2))
-#   print(fetch_aya(17, 78))
-#   print(fetch_aya(25, 9))
-#   print(fetch_aya(33, 73))
-#   print(fetch_aya(33, 1))
-#   print(fetch_aya(34, 1))
-#   print(fetch_aya(34, 54))
-#   print(fetch_aya(1, 1))
-#   print(fetch_aya(2, 1))
-#   print(fetch_aya(22, 77))
+    print(fetch_aya(12, 4))
+    print(fetch_aya(114, 2))
+    print(fetch_aya(17, 78))
+    print(fetch_aya(25, 9))
+    print(fetch_aya(33, 73))
+    print(fetch_aya(33, 1))
+    print(fetch_aya(34, 1))
+    print(fetch_aya(34, 54))
+    print(fetch_aya(1, 1))
+    print(fetch_aya(2, 1))
+    print(fetch_aya(22, 77))
 #   print(get_sura(1))
 #   print(get_sura(114))
 #   print(get_sura(113))
