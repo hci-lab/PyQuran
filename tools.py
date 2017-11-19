6c5cbcd0395280edf6ba553eddec2cd27c258dc0
TahaMagdy <tahamagdy314@gmail.com>
2017-11-19 03:37:23 +0200

    add the main module

---
+++ tools.py
@@ -0,0 +1,120 @@
+"""Main PyQuran Library  Module
+
+* Data: Sat Nov 18 03:30:41 EET 2017
+
+This module contains tools for `Quranic Analysis`
+(More expressive description later) 
+
+"""
+from xml.etree import ElementTree
+import numpy
+
+# Parsing xml
+xml_file_name = 'QuranCorpus/quran-simple-clean.xml'
+quran_tree = ElementTree.parse(xml_file_name)
+
+
+def get_sura(sura_number):
+    """gets an sura by returning a list of ayat al-sura.
+
+    Args: 
+        param1 (int): the ordered number of sura in The Mushaf.
+
+    Returns:
+         [str]: a list of ayat al-sura.
+
+    Usage Note:
+        Do not forget that the index of the reunred list starts at zero.
+        So if the order aya number is x, then it's at (x-1) in the list.
+
+    """
+    sura_number -= 1
+    sura = []
+    suras_list = quran_tree.findall('sura')
+    ayat = suras_list[sura_number]
+    for aya in ayat:
+        sura.append(aya.attrib['text'])
+    return sura
+
+
+
+def fetch_aya(sura_number, aya_number):
+    """
+
+    Args:
+        param1 (int): the ordered number of sura in The Mus'haf.
+        param2 (int): the ordered number of aya in The Mus'haf.
+
+    Returns:
+        str: an aya as a string
+
+    """
+    aya_number -= 1
+    sura = get_sura(sura_number)
+    return sura[aya_number]
+
+
+def parse_sura(n, alphabets=['ل', 'ب']):
+    """parses the sura and returns a matrix (ndarray),
+    the rows number equals to the ayat number,
+    and the columns number equals to the length of alphabets
+
+    What it does:
+        it calculates number of occurrences of each on of letters
+        in the alphabets for each aya.
+
+        If `A` is a ndarray, 
+        then A[i,j] is the number of occurrences of the letter 
+        alphabets[j] in the aya i.
+
+    Args:
+        param1 (int): the ordered number of sura in The Mus'haf.
+        param2 ([str]): a list of alphabets
+
+    Returns:
+        ndarray: with dimensions (a * m), where
+        `a` is the number of ayat el-sura and
+        `m` is the number of letters passed to the function through alphabets[]
+
+    Issue:
+        1. A list of Arabic letters maybe flipped by your editor,
+           so, the first char will be the most-right one,
+           unlike a list of English char, the first element 
+           is the left-most one.
+
+        2. I didn't 
+    
+
+    """
+    # getting the nth sura
+    sura  = get_sura(n)
+    # getting the ndarray dimensions 
+    a = len(sura)
+    m = len(alphabets)
+    # building ndarray with appropriate dimensions 
+    A = numpy.zeros((a,m), dtype=numpy.int)
+
+
+    # Filling ndarray with alphabets[] occurrences
+    i = 0 # number of current aya
+    j = 0 # occurrences
+    for aya in sura:
+        for letter in alphabets:
+            A[i,j] = aya.count(letter)
+            j += 1
+        j = 0
+        i += 1
+ 
+    print(A)
+    return
+
+
+
+def main():
+    # testing
+#    print(fetch_aya(10, 107))
+#    print(get_sura(10)[107-1])
+    parse_sura(111, ['م', 'ا', 'ب'])
+
+if __name__ == '__main__':
+    main()
