18cbb07d7936432ebcde20a44ce2924f6d2c1b0f
TahaMagdy <tahamagdy314@gmail.com>
2018-01-11 12:37:00 +0200

    uthmanic charachters Add all uthmanic charachters by their unicode as constants.

--- uthmanic.py
+++ uthmanic.py
@@ -1,12 +1,27 @@
-'''
-Contains Uthmanic symbols
+'''Contains Uthmanic symbols and related functions.
 '''
 
 '''
     TODO:
-        1 add all uthmanic  symbols by unicode, here
+        [DONE] add all uthmanic  symbols by unicode, here 
         2 add uthmanic_filter(symbols=[uthmanic]) -> uthmanic free text.
 
-        no kili and sili.
 '''
 
+hamza_above     = '\u0654'
+small_high_meem = '\u06e2'
+small_low_meem  = '\u06ed'
+small_high_seen = '\u06dc'
+small_low_seen  = '\u06e3'
+small_alef      = '\u0670'
+small_waw       = '\u06e5'
+small_yeh       = '\u06e6'
+small_high_noon = '\u06e8'
+mad_lazim_mark  = '\u0653'
+tatweel         = '\u0640'
+alef_wasl_with_saad_above   = '\u0671'
+empty_centre_high_stop      = '\u06eb'
+small_high_rounded_zero     = '\u06df'
+empty_center_low_stop       = '\u06ea'
+small_high_upright_rectangular_zero  = '\u06e0'
+rounded_high_stop_with_filled_centre = '\u06ec'
