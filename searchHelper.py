81b85d18429fbd9609001a57427e9202c25dcbd2
TahaMagdy <tahamagdy314@gmail.com>
2018-01-11 08:28:23 +0200

    add count_spaces_before_index()

--- searchHelper.py
+++ searchHelper.py
@@ -1,2 +1,20 @@
 """searchHelper: contains helper functions for searching.
 """
+
+
+def count_spaces_before_index(string, index):
+    """counts spaces before a char in string.
+
+    Args: 
+        param1 (str): string
+        param2 (int): char index inside string
+    Returns:
+         int: number of spaces before string[index]
+
+    """
+    count = 0
+    for i in range(index):
+        if string[i] == ' ':
+            count += 1
+
+    return count
