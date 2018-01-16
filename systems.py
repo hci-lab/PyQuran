9e703d5f35bdd8eda9af520734c95c66a68b03a3
TahaMagdy <tahamagdy314@gmail.com>
2018-01-16 19:25:44 +0200

    add two predefied systems

---
+++ systems.py
@@ -0,0 +1,15 @@
+from arabic import *
+
+
+# Exporting
+withoutDotSystem = [behLike]
+withoutDotSystem.append(jeemLike)
+withoutDotSystem.append(dalLike)
+withoutDotSystem.append(rehLike)
+withoutDotSystem.append(seenLike)
+withoutDotSystem.append(sadLike)
+withoutDotSystem.append(tahLike)
+withoutDotSystem.append(ainLike)
+withoutDotSystem.append(fehLike)
+
+hamazatSystem = [hamzat]
