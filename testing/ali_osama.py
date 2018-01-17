75bf6c4e93a1e62844cd0540543da79bf9f5c8b6
Ali Osama <ali.osama@fci.helwan.edu.eg>
2018-01-17 02:37:22 +0200

    fix a bug in separate_token_with_dicrites

--- testing/ali_osama.py
+++ testing/ali_osama.py
@@ -1,20 +1,23 @@
-
 from pyarabic.araby import strip_tashkeel, strip_tatweel
+
 def separate_token_with_dicrites(token):
-    """gets a token with taskeel, and returns a list contains the token characters with their tashkeel.
+    """gets a token(string) with taskeel, and returns a list of strings,
+    each string represents a character with its tashkeel.
     Args:
-        param1 (int): list contains the token characters with their tashkeel.
+        token (str): string represents a word or aya or sura
     Returns:
-         [str]: a list contains the token characters with their tashkeel.
+        [str]: a list contains all arabic alphabets with their tashkeel as strings.
     """
-    token_without_tatweel = strip_tatweel(token)
-    print(token_without_tatweel)
+    token_without_tatweel = araby.strip_tatweel(token)
     hroof_with_tashkeel = []
+    another_characters = [" ","\n"]
+    all_characters = list(arabic.alphabet) + list(arabic.alefat) + list(arabic.hamzat) + another_characters
+    all_harakat = list(arabic.tashkeel)+ list(arabic.harakat)+list(arabic.shortharakat)+list(arabic.tanwin)
     for index,i in enumerate(token):
-        if((token[index] in (alphabet or alefat or hamzat) )):
+        if(token[index] in all_characters):
             k = index
-            harf_with_taskeel =token[index]
-            while((k+1) != len(token) and (token[k+1] in (tashkeel or harakat or shortharakat or tanwin ))):
+            harf_with_taskeel = token[index]
+            while((k+1) != len(token) and token[k+1] in all_harakat):
                 harf_with_taskeel =harf_with_taskeel+""+token[k+1]
                 k = k + 1
             index = k
