b54d16bd270152b52849bfe8a378518083c1488b
TahaMagdy <tahamagdy314@gmail.com>
2018-02-07 06:53:13 +0200

    get_sura unittests

--- tools/test_quran.py
+++ tools/test_quran.py
@@ -3,8 +3,20 @@
 import unittest
 
 
+import quran
+
 class Testing_quran(unittest.TestCase):
-    pass
+
+    def test_get_sura(self):
+        self.assertEqual(len(quran.get_sura(1)), 7)
+        self.assertEqual(len(quran.get_sura(2)), 286)
+        self.assertEqual(len(quran.get_sura(49)), 18)
+        self.assertEqual(len(quran.get_sura(76)), 31)
+
+        self.assertEqual(len(quran.get_sura(1, True)), 7)
+        self.assertEqual(len(quran.get_sura(2, True)), 286)
+        self.assertEqual(len(quran.get_sura(49, True)), 18)
+        self.assertEqual(len(quran.get_sura(76, True)), 31)
 
 
 if __name__ == '__main__':
