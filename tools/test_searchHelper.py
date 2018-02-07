3cd58b32789078d63b65a561c953f5435da92694
TahaMagdy <tahamagdy314@gmail.com>
2018-02-07 07:26:11 +0200

    count_spaces_before_index unittests

--- tools/test_searchHelper.py
+++ tools/test_searchHelper.py
@@ -2,9 +2,13 @@
 """
 import unittest
 
-
+from searchHelper import *
 class Testing_searchHelper(unittest.TestCase):
-    pass
+
+    def test_count_spaces_before_index(self):
+        self.assertEqual(count_spaces_before_index('', 0), 0)
+        self.assertEqual(count_spaces_before_index('01 34 6 8', 8), 3)
+        self.assertEqual(count_spaces_before_index(' ', 1), 1)
 
 
 if __name__ == '__main__':
