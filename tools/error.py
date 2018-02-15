dde24c9f7774cd223fc0f428fb403a459c2befb0
TahaMagdy <tahamagdy314@gmail.com>
2018-02-15 18:43:45 +0200

    add is_string stderr

--- tools/error.py
+++ tools/error.py
@@ -9,3 +9,6 @@ def is_bool(boolean, message):
     if type(boolean) is not bool:
         raise ValueError(message)
 
+def is_string(string, message):
+    if type(string) is not string:
+        raise ValueError(message)
