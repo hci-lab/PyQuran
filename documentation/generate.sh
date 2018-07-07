78a0024f6f79d92db52b005fdf66441da62fc8d6
TahaMagdy <tahamagdy314@gmail.com>
2018-07-07 09:40:56 +0200

    syncing contribution.md

--- documentation/generate.sh
+++ documentation/generate.sh
@@ -4,6 +4,8 @@
 cat templates/analysis_tools_template.md > docs/analysis_tools.md
 cat templates/arabic_tools_template.md   > docs/arabic_tools.md
 cat templates/quran_tools_template.md    > docs/quran_tools.md
+cat docs/index.md                        > ../README.md
+cat docs/CONTRIBUTING.md                 > ../CONTRIBUTING.md
 
 
 # Generate docs
